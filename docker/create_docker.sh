67d3b3e005b93b0bdfa23a3010b92aa24e99dc0e
Michal Zerola <michal.zerola@gmail.com>
2017-04-04 21:54:57 +0200

    Fixing create_docker.sh script

--- docker/create_docker.sh
+++ docker/create_docker.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 # Copy the DAVe binaries
-cp -r -v ./target/dave-store-manager-1.0-SNAPSHOT/dave-store-manager-1.0-SNAPSHOT ./dockerfile/dave-store-manager-1.0-SNAPSHOT
+cp -r -v ./target/dave-store-manager-1.0-SNAPSHOT/dave-store-manager-1.0-SNAPSHOT ./docker/dave-store-manager-1.0-SNAPSHOT
 
 # Delete the prefilled
-rm -r ./dockerfile/dave-store-manager-1.0-SNAPSHOT/etc/storemanager.conf ./dockerfile/dave-store-manager-1.0-SNAPSHOT/etc/*.keystore ./dockerfile/dave-store-manager-1.0-SNAPSHOT/etc/*.truststore ./dockerfile/dave-store-manager-1.0-SNAPSHOT/etc/truststore
+rm -r ./docker/dave-store-manager-1.0-SNAPSHOT/etc/storemanager.conf
 docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-docker build -t dbgdave/dave-store-manager:${CIRCLE_SHA1} ./dockerfile/
-docker tag -f dbgdave/dave-store-manager${CIRCLE_SHA1} docker.io/dbgdave/dave-store-manager:${CIRCLE_SHA1}
-docker push dbgdave/dave-store-manager${CIRCLE_SHA1}
-docker tag -f dbgdave/dave-store-manager${CIRCLE_SHA1} docker.io/dbgdave/dave-store-manager:${CIRCLE_BRANCH}
-docker push dbgdave/dave-store-manager${CIRCLE_BRANCH}
+docker build -t dbgdave/dave-store-manager:${CIRCLE_SHA1} ./docker/
+docker tag -f dbgdave/dave-store-manager:${CIRCLE_SHA1} docker.io/dbgdave/dave-store-manager:${CIRCLE_SHA1}
+docker push dbgdave/dave-store-manager:${CIRCLE_SHA1}
+docker tag -f dbgdave/dave-store-manager:${CIRCLE_SHA1} docker.io/dbgdave/dave-store-manager:${CIRCLE_BRANCH}
+docker push dbgdave/dave-store-manager:${CIRCLE_BRANCH}
