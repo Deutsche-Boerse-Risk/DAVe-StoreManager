5ddfa1970ee6723cc68adb9a48c12540b09a09ac
Michal Zerola <michal.zerola@gmail.com>
2017-04-07 17:18:24 +0200

    Adding sample certificates into the config

--- docker/create_docker.sh
+++ docker/create_docker.sh
@@ -3,6 +3,9 @@
 # Copy the DAVe binaries
 cp -r -v ./target/dave-store-manager-1.0-SNAPSHOT/dave-store-manager-1.0-SNAPSHOT ./docker/dave-store-manager-1.0-SNAPSHOT
 
+sed -i 's/sslKey.*/sslKey\ =\ \"\"/' ./docker/dave-store-manager-1.0-SNAPSHOT/etc/storemanager.conf
+sed -i 's/sslCert.*/sslKey\ =\ \"\"/' ./docker/dave-store-manager-1.0-SNAPSHOT/etc/storemanager.conf
+
 docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 docker build -t dbgdave/dave-store-manager:${CIRCLE_SHA1} ./docker/
 docker tag -f dbgdave/dave-store-manager:${CIRCLE_SHA1} docker.io/dbgdave/dave-store-manager:${CIRCLE_SHA1}
